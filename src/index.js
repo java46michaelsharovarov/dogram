f1b5ec5574821298fea553e53a4cfbec741d31b0
Mik <m.shac86@gmail.com>
2022-01-02 18:37:18 +0200

    HW_7

---
+++ src/index.js
@@ -0,0 +1,14 @@
+console.log("script launcher");
+let detailsTitle = document.querySelector(".details-title");
+let detailsImage = document.querySelector(".details-image");
+let anchors = document.querySelectorAll(".thumbnails-anchor");
+for (let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener("click", function(event){
+        event.preventDefault();
+        setDetails(anchors[i]);
+    })
+}
+function setDetails(anchor){
+    detailsImage.setAttribute("src",anchor.getAttribute("href"));
+    detailsTitle.textContent = anchor.getAttribute("data-details-title");
+}
