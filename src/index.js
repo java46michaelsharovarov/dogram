ba93849edbc51012084c0eb91ce2ebd01f6c39c7
Mik <m.shac86@gmail.com>
2022-01-03 18:51:14 +0200

    HW_7

--- src/index.js
+++ src/index.js
@@ -10,6 +10,5 @@ for (let i = 0; i < anchors.length; i++) {
 }
 function setDetails(anchor){
     detailsImage.setAttribute("src",anchor.getAttribute("href"));
-    let title = anchor.querySelector(".thumbnails-title");    
-    detailsTitle.textContent = title.textContent + " : " + anchor.getAttribute("data-details-title");
+    detailsTitle.textContent = anchor.querySelector(".thumbnails-title").textContent + " : " + anchor.dataset.detailsTitle;
 }
